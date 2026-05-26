22c063d3cadc4ef15332d479bfc18464299d87cb
Crest Services <164747490+krokcuy@users.noreply.github.com>
2026-05-26 21:04:02 +0300

    Add files via upload

---
+++ SubTNTPreview.java
@@ -0,0 +1,6 @@
+public class SubTNTPreview {
+    public static void main(String[] args) {
+        System.out.println("Please read README.md file for details.");
+        System.out.println("This repository is only a preview. Full Java plugin source code is private.");
+    }
+}
